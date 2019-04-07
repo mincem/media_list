8ef3ad17ea6022863592281ea804b740ce4e9b96
Matías Incem <matiasincem@gmail.com>
2019-04-07 20:24:32 -0300

    Add ajax call for details

--- src/media_list/static/media_list/js/list.js
+++ src/media_list/static/media_list/js/list.js
@@ -1,5 +1,14 @@
-$(document).ready(function(){
-  $('.ml-row').click(function(){
-      window.location.href = $(this).data('detail-url');
-  });
+$(document).ready(function () {
+    $('.ml-row').click(function () {
+        $.ajax({
+            url: $(this).data('detail-url'),
+            type: 'get',
+            dataType: 'html',
+            success: function (data) {
+                let $detailsModal = $('#details-modal');
+                $detailsModal.find('.modal-content').html(data);
+                $detailsModal.modal('show');
+            }
+        });
+    });
 });
