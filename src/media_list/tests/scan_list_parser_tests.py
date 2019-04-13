9626151e1358da5d35af153c3a328f9885b8e429
Matías Incem <matiasincem@gmail.com>
2019-04-13 14:42:58 -0300

    Add functionality to scan list parser

--- src/media_list/tests/scan_list_parser_tests.py
+++ src/media_list/tests/scan_list_parser_tests.py
@@ -27,7 +27,7 @@ strike_the_blood_data = {
 
 water_dragon_bride_html = """
     <p style="margin-bottom: 0cm; line-height: 125%"><a
-        href="https://manga.madokami.al/Manga/S/SU/SUIJ/Suijin%20no%20Ikenie"><font color="#1155cc"><font
+        href="https://manga.madokami.al/Manga/S/SU/SUIJ/Suijin%20no%20Ikenie"><font color="#783f04"><font
         face="Arial, serif"><font size="2" style="font-size: 11pt"><u>Suijin
   no Hanayome</u></font></font></font></a><font face="Arial, serif"><font size="2" style="font-size: 11pt">
   [The Water Dragon's Bride] (7+ tomos)</font></font></p>
@@ -47,7 +47,7 @@ water_dragon_bride_data = {
 
 ceres_html = """
     <p style="margin-bottom: 0cm; line-height: 125%"><a
-            href="https://manga.madokami.al/Manga/A/AY/AYAS/Ayashi%20no%20Ceres"><font color="#1155cc"><font
+            href="https://manga.madokami.al/Manga/A/AY/AYAS/Ayashi%20no%20Ceres"><font color="#bf9000"><font
             face="Arial, serif"><font size="2" style="font-size: 11pt"><u>Ayashi
       no Ceres</u></font></font></font></a><font face="Arial, serif"><font size="2" style="font-size: 11pt">
       [Ceres Celestial Legend] (14 tomos, completo)&#127775;</font></font></p>
@@ -57,48 +57,97 @@ ceres_data = {
     "interest": 100,
 }
 
+beauty_html = """
+    <p style="margin-bottom: 0cm; line-height: 125%"><a
+            href="https://manga.madokami.al/Manga/B/BO/BOCC/Bocchi%20Kaibutsu%20to%20Moumoku%20Shoujo"><font
+            color="#1155cc"><font face="Arial, serif"><font size="2" style="font-size: 11pt"><u>Bocchi
+      Kaibutsu to Moumoku Shoujo</u></font></font></font></a><font face="Arial, serif"><font size="2"
+                                                                                             style="font-size: 11pt">
+      [Beauty and the Beast Girl] (unitario, yuri)</font></font></p>
+    """
+
+dragon_half_html = """
+    <p style="margin-bottom: 0cm; border: none; padding: 0cm; line-height: 125%">
+    <a href="https://manga.madokami.al/Manga/D/DR/DRAG/Dragon%20Half"><font color="#38761d">
+    <font face="Arial, serif"><font size="2" style="font-size: 11pt"><u>Dragon
+        Half</u></font></font></font></a><font face="Arial, serif"><font size="2" style="font-size: 11pt">
+    (3 omnibus, completo)&#127775;</font></font></p>
+    """
+
+another_html = """
+    <p style="margin-bottom: 0cm; line-height: 125%"><a href="https://manga.madokami.al/Manga/A/AN/ANOT/Another">
+    <font color="#1155cc"><font face="Arial, serif"><font size="2" style="font-size: 11pt">
+    <u>Another</u></font></font></font></a><font face="Arial, serif"><font size="2" style="font-size: 11pt">
+    (omnibus unitario)</font></font></p>
+    """
+
 
 class ScanListParserTests(TestCase):
     def test_store_correct_title(self):
         media_series = ScanListParser().scan_contents(strike_the_blood_html)[0]
-        self.assertEquals(media_series.title, strike_the_blood_data["title"])
+        self.assertEquals(strike_the_blood_data["title"], media_series.title)
 
     def test_store_correct_alternate_title(self):
         media_series = ScanListParser().scan_contents(water_dragon_bride_html)[0]
-        self.assertEquals(media_series.alternate_title, water_dragon_bride_data["alternate_title"])
+        self.assertEquals(water_dragon_bride_data["alternate_title"], media_series.alternate_title)
 
     def test_store_empty_alternate_title(self):
         media_series = ScanListParser().scan_contents(strike_the_blood_html)[0]
-        self.assertEquals(media_series.alternate_title, strike_the_blood_data["alternate_title"])
+        self.assertEquals(strike_the_blood_data["alternate_title"], media_series.alternate_title)
 
     def test_store_correct_url(self):
         media_series = ScanListParser().scan_contents(strike_the_blood_html)[0]
-        self.assertEquals(media_series.url, strike_the_blood_data["url"])
+        self.assertEquals(strike_the_blood_data["url"], media_series.url)
 
     def test_store_correct_volumes(self):
         media_series = ScanListParser().scan_contents(strike_the_blood_html)[0]
-        self.assertEquals(media_series.volumes, strike_the_blood_data["volumes"])
+        self.assertEquals(strike_the_blood_data["volumes"], media_series.volumes)
+
+    def test_standalone_stores_one_volume(self):
+        media_series = ScanListParser().scan_contents(beauty_html)[0]
+        self.assertEquals(1, media_series.volumes)
 
     def test_store_does_have_omnibus(self):
-        media_series = ScanListParser().scan_contents(strike_the_blood_html)[0]
-        self.assertEquals(media_series.has_omnibus, True)
+        media_series = ScanListParser().scan_contents(dragon_half_html)[0]
+        self.assertEquals(True, media_series.has_omnibus)
 
     def test_store_does_not_have_omnibus(self):
         media_series = ScanListParser().scan_contents(strike_the_blood_html)[0]
-        self.assertEquals(media_series.has_omnibus, False)
+        self.assertEquals(False, media_series.has_omnibus)
+
+    def test_store_standalone_omnibus(self):
+        media_series = ScanListParser().scan_contents(another_html)[0]
+        self.assertEquals(1, media_series.volumes)
+        self.assertEquals(True, media_series.has_omnibus)
 
     def test_store_is_completed(self):
         media_series = ScanListParser().scan_contents(strike_the_blood_html)[0]
-        self.assertEquals(media_series.is_completed, True)
+        self.assertEquals(True, media_series.is_completed)
 
     def test_store_is_not_completed(self):
         media_series = ScanListParser().scan_contents(water_dragon_bride_html)[0]
-        self.assertEquals(media_series.is_completed, False)
+        self.assertEquals(False, media_series.is_completed)
 
     def test_store_min_interest(self):
         media_series = ScanListParser().scan_contents(strike_the_blood_html)[0]
-        self.assertEquals(media_series.interest, MIN_INTEREST)
+        self.assertEquals(MIN_INTEREST, media_series.interest)
 
     def test_store_max_interest(self):
         media_series = ScanListParser().scan_contents(ceres_html)[0]
-        self.assertEquals(media_series.interest, MAX_INTEREST)
+        self.assertEquals(MAX_INTEREST, media_series.interest)
+
+    def test_store_status_not_downloaded(self):
+        media_series = ScanListParser().scan_contents(strike_the_blood_html)[0]
+        self.assertEquals("N", media_series.status)
+
+    def test_store_status_downloading(self):
+        media_series = ScanListParser().scan_contents(water_dragon_bride_html)[0]
+        self.assertEquals("D", media_series.status)
+
+    def test_store_status_downloaded_raw(self):
+        media_series = ScanListParser().scan_contents(ceres_html)[0]
+        self.assertEquals("R", media_series.status)
+
+    def test_store_status_edited(self):
+        media_series = ScanListParser().scan_contents(dragon_half_html)[0]
+        self.assertEquals("E", media_series.status)
--- src/media_list/utils/scan_list_parser.py
+++ src/media_list/utils/scan_list_parser.py
@@ -6,6 +6,14 @@ from ..models import MediaSeries
 
 STAR_EMOJI = "🌟"
 
+DEFAULT_LINK_COLOR = "#1155cc"
+LINK_COLORS = {
+    DEFAULT_LINK_COLOR: "N",
+    "#783f04": "D",
+    "#bf9000": "R",
+    "#38761d": "E",
+}
+
 
 class ScanListParser:
     def scan(self):
@@ -18,24 +26,17 @@ class ScanListParser:
 
     @staticmethod
     def extracted_data_from(entry):
-        url = parse_url(entry)
         line = entry_contents(entry)
-        title = re.search(r'(.*?) [(\[]', line).group(1)
-        alternate_title = line[line.find("[") + 1:line.find("]")] if "[" in line else ''
-        match = re.search(r'\((.*?)(\+?) (tomos|omnibus)', line)
-        volumes = 1 if "unitario" in line else int(match.group(1))
-        omnibus = "omnibus" in line
-        is_completed = "completo" in line
-        interest = 100 if STAR_EMOJI in line else 0
         return {
-            "title": title,
-            "alternate_title": alternate_title,
-            "url": url,
-            "volumes": volumes,
+            "title": parse_title(line),
+            "alternate_title": find_alternate_title(line),
+            "url": parse_url(entry),
+            "volumes": find_volumes(line),
             "source": 'E',
-            "has_omnibus": omnibus,
-            "is_completed": is_completed,
-            "interest": interest,
+            "has_omnibus": "omnibus" in line,
+            "is_completed": "completo" in line,
+            "interest": 100 if STAR_EMOJI in line else 0,
+            "status": find_status(entry),
             "notes": "",
         }
 
@@ -52,3 +53,29 @@ def entry_contents(entry):
 def parse_url(entry):
     hyperlink = entry.find("a")
     return None if hyperlink is None else hyperlink.get("href")
+
+
+def parse_title(line):
+    return re.search(r'(.*?) [(\[]', line).group(1)
+
+
+def find_alternate_title(line):
+    if "[" not in line:
+        return ""
+    return line[line.find("[") + 1:line.find("]")]
+
+
+def find_volumes(line):
+    if "unitario" in line:
+        return 1
+    match = re.search(r'\((.*?)(\+?) (tomos|omnibus)', line)
+    return int(match.group(1))
+
+
+def find_status(entry):
+    entry_html = str(entry)
+    try:
+        entry_text_color = next(color for color in (LINK_COLORS.keys()) if color in entry_html)
+    except StopIteration:
+        entry_text_color = DEFAULT_LINK_COLOR
+    return LINK_COLORS[entry_text_color]
