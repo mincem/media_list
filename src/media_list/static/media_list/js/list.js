db678e9ff70adcf1254c60b0250f4f1b0fa40c52
Matías Incem <matiasincem@gmail.com>
2019-05-04 20:35:02 -0300

    Refactor jQuery events

--- src/media_list/static/media_list/js/list.js
+++ src/media_list/static/media_list/js/list.js
@@ -1,32 +1,40 @@
 $(document).ready(function () {
-  $('.ml-row').click(function () {
-    $.ajax({
-      url: $(this).data('detail-url'),
-      type: 'get',
-      dataType: 'html',
-      success: function (data) {
-        let $detailsModal = $('#details-modal');
-        $detailsModal.find('.modal-content').html(data);
-        $detailsModal.modal('show');
-      }
-    });
-  });
+  $('.ml-row').click(showDetailsView);
 
-  $("#filters").find('.filter-title').on('keyup', function () {
-    let value = $(this).val().toLowerCase();
-    $('#media-list').find('tbody').find('tr').filter(function () {
-      $(this).toggle($(this).find('.ml-cell-title').text().toLowerCase().indexOf(value) > -1)
-    });
-  });
+  $("#filters").find('.filter-title').on('keyup', filterListByTitle);
 
-  $(".magic-button").click(function () {
-    $.ajax({
-      url: $(this).data('magic-url'),
-      type: 'get',
-      dataType: 'json',
-      success: function (data) {
-        alert(data['result']);
-      }
-    });
-  });
+  $(".magic-button").click(doMagic);
 });
+
+function showDetailsView() {
+  $.ajax({
+    url: $(this).data('detail-url'),
+    type: 'get',
+    dataType: 'html',
+    success: openDetailsModal
+  });
+}
+
+function openDetailsModal(data) {
+  let $detailsModal = $('#details-modal');
+  $detailsModal.find('.modal-content').html(data);
+  $detailsModal.modal('show');
+}
+
+function filterListByTitle(event) {
+  let searchInput = $(event.target).val().toLowerCase();
+  $('#media-list').find('tbody').find('tr').filter(function () {
+    $(this).toggle($(this).find('.ml-cell-title').text().toLowerCase().indexOf(searchInput) > -1)
+  });
+}
+
+function doMagic() {
+  $.ajax({
+    url: $(this).data('magic-url'),
+    type: 'get',
+    dataType: 'json',
+    success: function (data) {
+      alert(data['result']);
+    }
+  });
+}
