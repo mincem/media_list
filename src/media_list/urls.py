5e2c2def6914e48b933dfa71a242d4132b161c0d
Matías Incem <matiasincem@gmail.com>
2019-06-02 19:27:36 -0300

    Add delete series button

--- src/media_list/urls.py
+++ src/media_list/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     # ex: media_list/detail/5/
     path('detail/<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('edit/<int:pk>/', views.EditView.as_view(), name='edit'),
+    path('delete/<int:pk>/', views.DeleteView.as_view(), name='delete'),
     path('create/', views.CreateView.as_view(), name='create'),
     path('get_baka_id/<int:pk>/', views.FetchBakaIDView.as_view(), name='get_baka_id'),
     path('get_baka_info/<int:pk>/', views.FetchBakaInfoView.as_view(), name='get_baka_info'),
--- src/media_list/views.py
+++ src/media_list/views.py
@@ -25,24 +25,19 @@ class DetailView(generic.DetailView):
 class FetchBakaIDView(DetailView):
     def get(self, request, *args, **kwargs):
         series = self.get_object()
-        try:
-            series.baka_id = BakaFinder(series.title).baka_id()
-            series.save()
-        except Exception:
-            pass
+        series.baka_id = BakaFinder(series.title).baka_id()
+        series.save()
         return super().get(self, request, *args, **kwargs)
 
 
 class FetchBakaInfoView(DetailView):
     def get(self, request, *args, **kwargs):
         series = self.get_object()
-        try:
-            baka_info = BakaParser(series.baka_id).perform()
-            series.baka_info = baka_info
-            series.save()
-        except Exception:
-            pass
+        baka_info = BakaParser(series.baka_id).perform()
+        series.baka_info = baka_info
+        series.save()
         return super().get(self, request, *args, **kwargs)
+
 
 class SwapMangaSeriesTitlesView(DetailView):
     def get(self, request, *args, **kwargs):
@@ -61,4 +56,9 @@ class EditView(generic.UpdateView):
     model = MangaSeries
     form_class = MangaSeriesCreateForm
     template_name = "media_list/forms/manga_series_edit_form.html"
+    success_url = reverse_lazy("index")
+
+
+class DeleteView(generic.DeleteView):
+    model = MangaSeries
     success_url = reverse_lazy("index")
