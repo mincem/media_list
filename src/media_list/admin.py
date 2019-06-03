117b205703ab6534995a0ddb882a1410a50b1097
Matías Incem <matiasincem@gmail.com>
2019-06-02 21:59:50 -0300

    Add URL inlines to admin

--- src/media_list/admin.py
+++ src/media_list/admin.py
@@ -1,9 +1,38 @@
 from django.contrib import admin
 
-from .models import MangaSeries, BakaSeries, MangaSource, MangaGenre, MangaPerson
+from .models import MangaSeries, BakaSeries, MangaSource, MangaGenre, MangaPerson, MangaURL
 
-admin.site.register(MangaSeries)
-admin.site.register(BakaSeries)
-admin.site.register(MangaSource)
-admin.site.register(MangaGenre)
-admin.site.register(MangaPerson)
+
+class URLInline(admin.TabularInline):
+    model = MangaURL
+
+
+@admin.register(MangaSeries)
+class MangaSeriesAdmin(admin.ModelAdmin):
+    inlines = [URLInline]
+    search_fields = ['title']
+
+
+@admin.register(BakaSeries)
+class BakaSeriesAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(MangaSource)
+class MangaSourceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(MangaGenre)
+class MangaGenreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(MangaPerson)
+class MangaPersonAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(MangaURL)
+class MangaURLAdmin(admin.ModelAdmin):
+    pass
