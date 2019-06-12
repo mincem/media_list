269928c91f8fc1443a8f35b8e40d366526600f89
Matías Incem <matiasincem@gmail.com>
2019-06-11 22:08:22 -0300

    Test forms

---
+++ src/media_list/tests/test_manga_series_forms.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+
+from ..models import MangaSeries
+
+
+class MangaSeriesFormsTests(TestCase):
+    def setUp(self):
+        self.title = "Title"
+        self.alternate_title = "Alt"
+
+    def test_item_fields_are_swapped(self):
+        ms = MangaSeries.objects.create(
+            title=self.title,
+            alternate_title=self.alternate_title,
+            interest=2,
+        )
+        ms.title, ms.alternate_title = ms.alternate_title, ms.title
+
+        self.assertEquals(self.title, ms.alternate_title)
+        self.assertEquals(self.alternate_title, ms.title)
