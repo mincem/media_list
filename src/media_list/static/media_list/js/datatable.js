afa706fb1e858b98110928189b218cca0b67265d
Matías Incem <matiasincem@gmail.com>
2019-08-31 17:31:18 -0300

    Add new search function

--- src/media_list/static/media_list/js/datatable.js
+++ src/media_list/static/media_list/js/datatable.js
@@ -1,20 +1,59 @@
 $(document).ready(function () {
-  $('#media-list-table').DataTable(dataTableOptions);
+  let table = $('#media-list-table').DataTable(dataTableOptions);
+
+  $('#list-filter-by-title').on('keyup', function () {
+    table.search(this.value).draw();
+  });
 });
 
-dataTableOptions = {
-  'columns': [
-    {'name': 'image', 'orderable': false},
-    {'name': 'source', 'orderable': false},
-    {'name': 'title', 'orderable': true},
-    {'name': 'volumes', 'orderable': true},
-    {'name': 'completed', 'orderable': false},
-    {'name': 'interest', 'orderable': true},
-    {'name': 'status', 'orderable': true},
-    {'name': 'actions', 'orderable': false}
-  ],
-  'order': [[2, 'asc']],
-  'paging': false,
-  'scrollY': '70vh',
-  'scrollCollapse': true,
+let dataTableColumns = [
+  {
+    name: 'image',
+    orderable: false,
+    searchable: false,
+  },
+  {
+    name: 'source',
+    orderable: false,
+    searchable: false,
+  },
+  {
+    name: 'title',
+    orderable: true,
+    searchable: true,
+  },
+  {
+    name: 'volumes',
+    orderable: true,
+    searchable: false,
+  },
+  {
+    name: 'completed',
+    orderable: false,
+    searchable: false,
+  },
+  {
+    name: 'interest',
+    orderable: true,
+    searchable: false
+  },
+  {
+    name: 'status',
+    orderable: true,
+    searchable: false,
+  },
+  {
+    name: 'actions',
+    orderable: false,
+    searchable: false,
+  }
+];
+
+let dataTableOptions = {
+  columns: dataTableColumns,
+  order: [[2, 'asc']],
+  paging: false,
+  scrollY: '72vh',
+  scrollCollapse: true,
+  sDom: 'ltipr', // Everything except search box
 };
