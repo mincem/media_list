f0b962c2f497786c78d437c3597697daa86ffd83
Matías Incem <matiasincem@gmail.com>
2019-11-20 20:55:50 -0300

    Refactor filter events

--- src/media_list/static/media_list/js/datatable.js
+++ src/media_list/static/media_list/js/datatable.js
@@ -5,22 +5,50 @@ class DataTableView {
   }
 
   bindEvents() {
+    this.bindFilterByTitle();
+    this.bindFilterByStaff();
+    this.bindFilterBySource();
+    this.bindFilterByCompleted();
+    this.bindFilterByBakaInfo();
+  }
+
+  bindFilterByTitle() {
     $('#list-filter-by-title').keyup((event) => {
       this.table.column('title:name').search(event.target.value).draw();
     });
+    $('#list-filter-by-title-clear').click(() => {
+      $('#list-filter-by-title').val('').focus();
+      this.table.column('title:name').search('').draw();
+    });
+  }
+
+  bindFilterByStaff() {
     $('#list-filter-by-staff').keyup((event) => {
       this.table.column('staff:name').search(event.target.value).draw();
     });
+    $('#list-filter-by-staff-clear').click(() => {
+      $('#list-filter-by-staff').val('').focus();
+      this.table.column('staff:name').search('').draw();
+    });
+  }
+
+  bindFilterBySource() {
     $('#list-filter-by-source').change((event) => {
       this.table.column('source:name').search(event.target.value).draw();
     });
+  }
+
+  bindFilterByCompleted() {
     $('#list-filter-by-completed').change((event) => {
       this.table.column('completed:name').search(event.target.value).draw();
     });
+  }
+
+  bindFilterByBakaInfo() {
     $('#list-filter-by-baka-info').change((event) => {
       var searchValue;
-      if(event.target.value === 'True') searchValue = '\S';
-      else if(event.target.value === 'False') searchValue = '^$';
+      if (event.target.value === 'True') searchValue = '\S';
+      else if (event.target.value === 'False') searchValue = '^$';
       else searchValue = '';
       this.table.column('staff:name').search(searchValue, true).draw();
     });
