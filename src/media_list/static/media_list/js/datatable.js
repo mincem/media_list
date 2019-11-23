12ba6280011a15c818d5383927d72dfd89d70f40
Matías Incem <matiasincem@gmail.com>
2019-11-23 17:32:01 -0300

    Set width for small columns

--- src/media_list/static/media_list/js/datatable.js
+++ src/media_list/static/media_list/js/datatable.js
@@ -60,11 +60,13 @@ class DataTableView {
         name: 'image',
         orderable: false,
         searchable: false,
+        width: '10px',
       },
       {
         name: 'source',
         orderable: false,
         searchable: true,
+        width: '10px',
       },
       {
         name: 'title',
@@ -80,6 +82,7 @@ class DataTableView {
         name: 'year',
         orderable: true,
         searchable: false,
+        width: '10px',
       },
       {
         name: 'volumes',
@@ -89,17 +92,20 @@ class DataTableView {
       {
         name: 'interest',
         orderable: true,
-        searchable: false
+        searchable: false,
+        width: '10px',
       },
       {
         name: 'status',
         orderable: true,
         searchable: false,
+        width: '10px',
       },
       {
         name: 'actions',
         orderable: false,
         searchable: false,
+        width: '10px',
       }
     ];
   }
