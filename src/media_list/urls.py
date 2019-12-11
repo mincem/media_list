327af66e64ae827ee8112b8730abe661efe47fa3
Matías Incem <matiasincem@gmail.com>
2019-12-10 23:29:39 -0300

    Move URL primary key to the front

--- src/media_list/urls.py
+++ src/media_list/urls.py
@@ -4,15 +4,15 @@ from . import views
 
 manga_urls = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.IndexView.as_view(), name='index_and_modal'),
-    path('detail/<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('edit/<int:pk>/', views.EditView.as_view(), name='edit'),
-    path('edit_interest/<int:pk>/', views.EditInterestView.as_view(), name='edit_interest'),
-    path('delete/<int:pk>/', views.DeleteView.as_view(), name='delete'),
     path('create/', views.CreateView.as_view(), name='create'),
-    path('get_baka_id/<int:pk>/', views.FetchBakaIDView.as_view(), name='get_baka_id'),
-    path('get_baka_info/<int:pk>/', views.FetchBakaInfoView.as_view(), name='get_baka_info'),
-    path('swap_titles/<int:pk>/', views.SwapMangaSeriesTitlesView.as_view(), name='swap_titles'),
+    path('<int:pk>/', views.IndexView.as_view(), name='index_and_modal'),
+    path('<int:pk>/detail/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/edit/', views.EditView.as_view(), name='edit'),
+    path('<int:pk>/edit_interest/', views.EditInterestView.as_view(), name='edit_interest'),
+    path('<int:pk>/delete/', views.DeleteView.as_view(), name='delete'),
+    path('<int:pk>/get_baka_id/', views.FetchBakaIDView.as_view(), name='get_baka_id'),
+    path('<int:pk>/get_baka_info/', views.FetchBakaInfoView.as_view(), name='get_baka_info'),
+    path('<int:pk>/swap_titles/', views.SwapMangaSeriesTitlesView.as_view(), name='swap_titles'),
 ]
 
 urlpatterns = [
