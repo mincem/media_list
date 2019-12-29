b9ab2d15ac36a9cbe6d87600dcc6349d3ea68882
Matías Incem <matiasincem@gmail.com>
2019-12-29 13:44:17 -0300

    Extract mangaupdates scraper

---
+++ src/media_list/utils/baka_page_scraper.py
@@ -0,0 +1,98 @@
+import re
+
+from bs4 import BeautifulSoup
+
+
+class BakaPageScraper:
+    def __init__(self, page_html, baka_id, image_retriever):
+        self.baka_id = baka_id
+        self.main_content = BeautifulSoup(page_html, "lxml").find(id="main_content")
+        self.contents = self.all_contents()
+        self.image_retriever = image_retriever
+
+    def all_contents(self):
+        return {name_of(category): html_of(category) for category in self.main_content(class_="sCat")}
+
+    def parse(self):
+        return {
+            "baka_id": self.baka_id,
+            "title": self.parse_title(),
+            "genre_names": self.parse_genres(),
+            "keywords": self.parse_keywords(),
+            "description": self.parse_description(),
+            "status": self.parse_status(),
+            "author_names": self.parse_authors(),
+            "artist_names": self.parse_artists(),
+            "year": int(clean_text(self.contents["Year"])) or None,
+            "original_publisher": clean_text(self.contents["Original Publisher"]) or "",
+            "english_publisher": clean_text(self.contents["English Publisher"]) or "",
+            "image": self.parse_image(self.contents["Image [ Report Inappropriate Content ]"]),
+        }
+
+    def parse_title(self):
+        return self.main_content.find(class_="releasestitle").string
+
+    def parse_genres(self):
+        genres = self.contents["Genre"]
+        if genres is None:
+            return []
+        genres.br.find_next_sibling().extract()
+        return list(genres.stripped_strings)
+
+    def parse_keywords(self):
+        keywords = self.contents["Categories"]
+        if keywords is None:
+            return []
+        return [self.parse_keyword(item) for item in keywords.find("ul").find_all("li")]
+
+    @staticmethod
+    def parse_keyword(html):
+        return {
+            "name": clean(html.text),
+            "score": re.search(r'Score: (.*) \(', html.a.attrs["title"]).group(1)
+        }
+
+    def parse_description(self):
+        description_tag = self.main_content.find(id="div_desc_more")
+        if description_tag is None:
+            description_tag = self.contents["Description"]
+        else:
+            description_tag.find_all("a")[-1].extract()
+        return clean_text(description_tag)
+
+    def parse_status(self):
+        return clean_text(self.contents["Status in Country of Origin"])
+
+    def parse_authors(self):
+        return self.parse_person_names(section="Author(s)")
+
+    def parse_artists(self):
+        return self.parse_person_names(section="Artist(s)")
+
+    def parse_person_names(self, section):
+        section_html = self.contents[section]
+        if section_html is None:
+            return []
+        return [hyperlink.string for hyperlink in section_html.find_all("a")]
+
+    def parse_image(self, html_tag):
+        if html_tag.img is None:
+            return None
+        image_url = html_tag.img["src"]
+        return self.image_retriever.get(image_url)
+
+
+def name_of(category):
+    return " ".join(category.stripped_strings)
+
+
+def html_of(category):
+    return category.find_next_sibling(class_="sContent")
+
+
+def clean_text(html_tag):
+    return clean(" ".join(html_tag.stripped_strings))
+
+
+def clean(text):
+    return " ".join(text.split())
--- src/media_list/utils/baka_parser.py
+++ src/media_list/utils/baka_parser.py
@@ -1,11 +1,10 @@
 import json
 import os
-import re
 
 import requests
-from bs4 import BeautifulSoup
 from django.core.files.base import ContentFile
 
+from .baka_page_scraper import BakaPageScraper
 from ..repositories import BakaSeriesRepository
 
 
@@ -25,93 +24,12 @@ class BakaParser:
         return json.dumps(self.parsed_series_data(), indent=2)
 
     def parsed_series_data(self):
-        soup = BeautifulSoup(self.baka_web_page_html(), "lxml")
-        main_content = soup.find(id="main_content")
-        contents = all_contents(main_content)
-        return {
-            "baka_id": self.baka_id,
-            "title": parse_title(main_content),
-            "genre_names": parse_genres(contents["Genre"]) or [],
-            "keywords": parse_keywords(contents["Categories"]) or [],
-            "description": parse_description(main_content, contents),
-            "status": clean_text(contents["Status in Country of Origin"]),
-            "author_names": parse_person_names(contents["Author(s)"]),
-            "artist_names": parse_person_names(contents["Artist(s)"]),
-            "year": int(clean_text(contents["Year"])) or None,
-            "original_publisher": clean_text(contents["Original Publisher"]) or "",
-            "english_publisher": clean_text(contents["English Publisher"]) or "",
-            "image": self.parse_image(contents["Image [ Report Inappropriate Content ]"]),
-        }
+        return BakaPageScraper(self.baka_web_page_html(), self.baka_id, self.image_retriever).parse()
 
     def baka_web_page_html(self):
         if self.web_page_html is None:
             self.web_page_html = self.baka_retriever.get(self.baka_id)
         return self.web_page_html
-
-    def parse_image(self, html_tag):
-        if html_tag.img is None:
-            return None
-        image_url = html_tag.img["src"]
-        return self.image_retriever.get(image_url)
-
-
-def all_contents(soup):
-    return {name_of(category): html_of(category) for category in soup(class_="sCat")}
-
-
-def name_of(category):
-    return " ".join(category.stripped_strings)
-
-
-def html_of(category):
-    return category.find_next_sibling(class_="sContent")
-
-
-def parse_title(soup):
-    return soup.find(class_="releasestitle").string
-
-
-def parse_description(soup, contents):
-    description_tag = soup.find(id="div_desc_more")
-    if description_tag is None:
-        description_tag = contents["Description"]
-    else:
-        description_tag.find_all("a")[-1].extract()
-    return clean_text(description_tag)
-
-
-def parse_person_names(html):
-    if html is None:
-        return []
-    return [hyperlink.string for hyperlink in html.find_all("a")]
-
-
-def parse_genres(html):
-    if html is None:
-        return None
-    html.br.find_next_sibling().extract()
-    return list(html.stripped_strings)
-
-
-def parse_keywords(html):
-    if html is None:
-        return None
-    return [parse_keyword(item) for item in html.find("ul").find_all("li")]
-
-
-def parse_keyword(html):
-    return {
-        "name": clean(html.text),
-        "score": re.search(r'Score: (.*) \(', html.a.attrs["title"]).group(1)
-    }
-
-
-def clean_text(html_tag):
-    return clean(" ".join(html_tag.stripped_strings))
-
-
-def clean(text):
-    return " ".join(text.split())
 
 
 class BakaRetriever:
