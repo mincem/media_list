daa94ff97e6f64364a3cbc5ef58c435a528ae8d6
Matías Incem <matiasincem@gmail.com>
2020-01-01 14:10:43 -0300

    Add command to fetch keywords for old manga series

---
+++ src/media_list/management/commands/get_missing_keywords.py
@@ -0,0 +1,30 @@
+from time import sleep
+
+from django.core.management.base import BaseCommand
+
+from ...models import BakaSeries, MangaKeyword
+from ...utils import BakaPageScraper, BakaRetriever
+
+
+class Command(BaseCommand):
+    help = 'Fetches Mangaupdates keywords for old BakaSeries models'
+
+    def handle(self, *args, **options):
+        for baka_series in BakaSeries.objects.all():
+            if baka_series.keywords.count():
+                print(f'Series "{baka_series.title}" already has keywords')
+            else:
+                print(f'Adding keywords to series "{baka_series.title}"... ')
+                self.fetch_keywords(baka_series)
+                print(f'\rAdded keywords to series "{baka_series.title}"')
+                sleep(5)
+
+
+    def fetch_keywords(self, baka_series):
+        keywords_data = BakaPageScraper(
+            page_html=BakaRetriever().get(baka_series.baka_id),
+            baka_id=baka_series.baka_id,
+        ).parse()["keywords"]
+        for keyword_data in keywords_data:
+            keyword = MangaKeyword.objects.get_or_create(name=keyword_data["name"])[0]
+            baka_series.keywords.add(keyword, through_defaults={"score": keyword_data["score"]})
