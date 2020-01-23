068683297cd111c5fd96e24a047039b1912fbbc1
Matías Incem <matiasincem@gmail.com>
2020-01-23 15:41:13 -0300

    Change database path

--- src/config/settings/development.py
+++ src/config/settings/development.py
@@ -3,6 +3,6 @@ from .base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db/db_test.sqlite3'),
     }
 }
