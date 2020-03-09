173a3fcabba1747a9e2d008fa4ad6f83f934a426
Matías Incem <matiasincem@gmail.com>
2020-03-09 20:51:29 -0300

    Refactor import link list command

--- src/media_list/management/commands/import_link_list.py
+++ src/media_list/management/commands/import_link_list.py
@@ -15,13 +15,13 @@ class Command(BaseCommand):
         parser.add_argument('source', type=str)
 
     def handle(self, *args, **options):
-        results = self.parse(options["file"], options["source"])
+        self.source = MangaSource.objects.get_or_create(name=options["source"])[0]
+        self.parse(options["file"])
 
     def print_dividing_line(self):
         self.stdout.write(self.style.ERROR("----------------------------------------------------"))
 
-    def parse(self, filename, source_name):
-        source = MangaSource.objects.get_or_create(name=source_name)[0]
+    def parse(self, filename):
         if not os.path.isfile(filename):
             raise FileNotFoundError(f'File "{filename}" does not exist.')
         with open(filename, 'r') as html_file:
@@ -31,43 +31,50 @@ class Command(BaseCommand):
     def scan_contents(self, line):
         markdown_link_pattern = re.compile(r"\[([\w\W\s\d]+?)\]\(((?:/|https?://)[\w\d./?=#]+)\)")
         for match in re.finditer(markdown_link_pattern, line):
-            text = match.group(1)
-            url = match.group(2)
-            full_text, title, volumes = self.parse_text(text)
-            if title:
-                print(f"text: {full_text}")
-                print(f"title: {title}")
-                print(f"volumes: {volumes}")
-
-                manga_best_match = self.best_match(title)
-                answer = input("Proceed?\n")
-                if answer == 'y':
-                    if manga_best_match:
-                        self.add_url(manga_best_match, "")
-
+            parse_entry(
+                text=match.group(1),
+                url=match.group(2)
+            )
         self.print_dividing_line()
 
-    def parse_text(self, text):
-        pattern = re.compile(r"(.*?)\(?[ v[\d]+-?([\d]+)]?")
-        match = re.match(pattern, text)
-        if match is None:
-            return
-        full_text = match.group(0)
-        title = match.group(1)
-        volumes = match.group(2)
-        return full_text, title, volumes
 
-    def best_match(self, title):
+def parse_entry(text, url):
+    full_text, title, volumes = parse_text(text)
+    if title:
+        print(f"text: {full_text}")
+        print(f"title: {title}")
+        print(f"volumes: {volumes}")
+
+        manga_best_match = best_match(title)
+        answer = input("Proceed?\n")
+        if answer == 'y':
+            if manga_best_match:
+                add_url(manga_best_match, url)
+
+
+def parse_text(text):
+    pattern = re.compile(r"(.*?)\(?[ v[\d]+-?([\d]+)]?")
+    match = re.match(pattern, text)
+    if match is None:
+        return
+    full_text = match.group(0)
+    title = match.group(1)
+    volumes = match.group(2)
+    return full_text, title, volumes
+
+
+def add_url(manga, url):
+    manga.urls.create(url=url)
+
+
+def best_match(title):
+    try:
+        manga = MangaSeries.objects.get(title__icontains=title)
+    except ObjectDoesNotExist:
         try:
-            manga = MangaSeries.objects.get(title__icontains=title)
+            manga = MangaSeries.objects.get(alternate_title__icontains=title)
         except ObjectDoesNotExist:
-            try:
-                manga = MangaSeries.objects.get(alternate_title__icontains=title)
-            except ObjectDoesNotExist:
-                manga = None
-        if manga:
-            print(f"best match: {manga} ({manga.volumes} volumes)")
-        return manga
-
-    def add_url(self, manga, url):
-        manga.urls.create(url=url)
+            manga = None
+    if manga:
+        print(f"best match: {manga} ({manga.volumes} volumes)")
+    return manga
