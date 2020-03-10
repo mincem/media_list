012323fb34b5a7a358d771e791ea6d65ee5ef9e2
Matías Incem <matiasincem@gmail.com>
2020-03-10 20:48:48 -0300

    Offer choice of updating best match or creating new one

--- src/media_list/management/commands/import_link_list.py
+++ src/media_list/management/commands/import_link_list.py
@@ -31,27 +31,43 @@ class Command(BaseCommand):
     def scan_contents(self, line):
         markdown_link_pattern = re.compile(r"\[([\w\W\s\d]+?)\]\(((?:/|https?://)[\w\d./?=#]+)\)")
         for match in re.finditer(markdown_link_pattern, line):
-            parse_entry(
+            self.parse_entry(
                 text=match.group(1),
                 url=match.group(2)
             )
         self.print_dividing_line()
 
+    def parse_entry(self, text, url):
+        full_text, title, volumes = parse_text(text)
+        print(f"text: {full_text}")
+        if not title:
+            print("No data extracted")
+            return
+        print(f"title: {title}")
+        print(f"volumes: {volumes}")
+        best_match = manga_best_match(title)
+        if best_match:
+            print(f"best match: {best_match} ({best_match.volumes} volumes)")
+        self.process_manga(title, volumes, url, best_match)
 
-def parse_entry(text, url):
-    full_text, title, volumes = parse_text(text)
-    print(f"text: {full_text}")
-    if not title:
-        print("No data extracted")
-        return
-    print(f"title: {title}")
-    print(f"volumes: {volumes}")
-    best_match = manga_best_match(title)
-    if best_match:
-        print(f"best match: {best_match} ({best_match.volumes} volumes)")
-    answer = input("Proceed?\n")
-    if answer == 'y':
-        process_manga(title, volumes, url, best_match)
+    def process_manga(self, title, volumes, url, best_match):
+        if best_match:
+            answer = input("Update best match?\n")
+            if answer == 'y':
+                return update_best_match(best_match, volumes, url)
+        answer = input("Add new manga?\n")
+        if answer == 'y':
+            return self.save_new_manga(title, volumes, url)
+
+    def save_new_manga(self, title, volumes, url):
+        manga = MangaSeries.objects.create(
+            title=title,
+            volumes=volumes,
+            interest=0,
+            status='N',
+            source=self.source
+        )
+        add_url(manga, url)
 
 
 def parse_text(text):
@@ -66,31 +82,16 @@ def parse_text(text):
 
 
 def manga_best_match(title):
-    try:
-        return MangaSeries.objects.get(title__icontains=title)
-    except ObjectDoesNotExist:
-        try:
-            return MangaSeries.objects.get(alternate_title__icontains=title)
-        except ObjectDoesNotExist:
-            return
+    query = MangaSeries.objects.filter(title__icontains=title) | \
+            MangaSeries.objects.filter(alternate_title__icontains=title)
+    return query.first()
 
 
-def process_manga(title, volumes, url, best_match):
-    if best_match:
-        add_url(best_match, url)
-    else:
-        save_new_manga(title, volumes, url)
-
-
-def save_new_manga(title, volumes, url):
-    manga = MangaSeries.objects.create(
-        title=title,
-        volumes=volumes,
-        interest=0,
-        status='N',
-    )
-    add_url(manga, url)
+def update_best_match(best_match, volumes, url):
+    best_match.volumes = volumes
+    best_match.save()
+    add_url(best_match, url)
 
 
 def add_url(manga, url):
-    manga.urls.create(url=url)
+    manga.urls.get_or_create(url=url)
