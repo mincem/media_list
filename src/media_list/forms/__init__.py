fffb1534100c8961fe1ec93a4a4a39f8f8f3f850
Matías Incem <matiasincem@gmail.com>
2020-07-27 22:51:31 -0300

    Display items one at a time

--- src/media_list/forms/__init__.py
+++ src/media_list/forms/__init__.py
@@ -1,3 +1,5 @@
 from .base_forms import MediaItemForm
 from .manga_forms import MangaForm, MangaURLInline
 from .movie_forms import MovieForm, MovieURLInline
+
+from .batch_importer_forms import BatchImporterInputForm
