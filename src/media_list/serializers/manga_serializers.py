b76a841e85177c4898d58837f9cc2cf34946fd16
Matías Incem <matiasincem@gmail.com>
2020-08-15 13:57:25 -0300

    Delete urls from serializer

--- src/media_list/serializers/manga_serializers.py
+++ src/media_list/serializers/manga_serializers.py
@@ -9,14 +9,25 @@ class MangaURLSerializer(serializers.ModelSerializer):
         fields = ["id", "url"]
 
 
+class UrlsToDeleteSerializer(serializers.ListField):
+    child = serializers.IntegerField()
+
+    def get_attribute(self, instance):
+        pass
+
+
 class MangaSerializer(serializers.ModelSerializer):
     urls = MangaURLSerializer(many=True, allow_null=True, required=False)
+    urls_to_delete = UrlsToDeleteSerializer()
 
     class Meta:
         model = MangaSeries
-        fields = ["id", "title", "alternate_title", "volumes", "interest", "status", "is_completed", "urls"]
+        fields = [
+            "id", "title", "alternate_title", "volumes", "interest", "status", "is_completed", "urls", "urls_to_delete"
+        ]
 
     def create(self, validated_data):
+        validated_data.pop("urls_to_delete", [])
         urls_data = validated_data.pop("urls", [])
         manga = MangaSeries.objects.create(**validated_data)
         for url_data in urls_data:
@@ -25,9 +36,16 @@ class MangaSerializer(serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
         urls_data = validated_data.pop("urls", [])
+        urls_to_delete = validated_data.pop("urls_to_delete", [])
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
         instance.save()
         for url_data in urls_data:
             instance.urls.get_or_create(**url_data)
+        self.delete_urls(instance, urls_to_delete)
         return instance
+
+    def delete_urls(self, manga, url_ids):
+        urls = manga.urls
+        urls_filter = urls.filter(pk__in=url_ids)
+        urls_filter.delete()
