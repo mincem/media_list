4e42c31ed5e9445a34ad92f9ce02205637a326f2
Matías Incem <matiasincem@gmail.com>
2023-06-20 21:53:48 -0300

    Clean some lint

--- src/media_list/models/base_models.py
+++ src/media_list/models/base_models.py
@@ -17,7 +17,7 @@ class NamedModel(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
 
 class MediaItem(TimestampedModel):
