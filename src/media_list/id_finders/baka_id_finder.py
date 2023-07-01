5befb35afcf4140d01716c3c0901aa7436b38a06
Matías Incem <matiasincem@gmail.com>
2023-06-30 22:44:12 -0300

    Add tests to ID finder

--- src/media_list/id_finders/baka_id_finder.py
+++ src/media_list/id_finders/baka_id_finder.py
@@ -16,14 +16,11 @@ class BakaIDFinder(ExternalIDFinder):
         return isinstance(item, MangaSeries)
 
     def get_id(self):
-        link = self.fetch_link()
+        link = self.link_fetcher.fetch(self.title)
         url = urlparse(link)
         if not self.source_is_correct(url):
             raise Exception(f"Cannot get Baka-ID from URL: {link}")
         return int(parse_qs(url.query)["id"][0])
-
-    def fetch_link(self):
-        return self.link_fetcher.get(self.title)
 
     @staticmethod
     def source_is_correct(url):
@@ -32,7 +29,7 @@ class BakaIDFinder(ExternalIDFinder):
 
 class LinkFetcher:
     @staticmethod
-    def get(series_title):
+    def fetch(series_title):
         for url in googlesearch.search(f"site:mangaupdates.com {series_title}", stop=20):
             return url
         raise Exception(f'Cannot find Mangaupdates page for series "{series_title}"')
---
+++ src/media_list/tests/id_finders/test_baka_id_finder.py
@@ -0,0 +1,44 @@
+from django.test import TestCase
+
+from ...id_finders import BakaIDFinder
+
+SERIES_TITLE = "Some Title"
+
+NUMERIC_ID = 12345
+ALPHANUMERIC_ID = "a1b2c3d4"
+
+NUMERIC_ID_URL = f"https://www.mangaupdates.com/series.html?id={NUMERIC_ID}"
+ALPHANUMERIC_ID_URL = f"https://www.mangaupdates.com/series/{ALPHANUMERIC_ID}"
+
+
+class TestBakaIDFinder(TestCase):
+    def test_get_id_from_numeric_url(self):
+        link_fetcher = MockLinkFetcher(NUMERIC_ID_URL)
+        finder = BakaIDFinder(SERIES_TITLE, link_fetcher)
+        self.assertEqual(NUMERIC_ID, finder.get_id())
+
+    def test_get_id_from_alphanumeric_url(self):
+        link_fetcher = MockLinkFetcher(ALPHANUMERIC_ID_URL)
+        finder = BakaIDFinder(SERIES_TITLE, link_fetcher)
+        self.assertEqual(ALPHANUMERIC_ID, finder.get_id())
+
+    def test_get_id_from_different_host_url(self):
+        wrong_url = "https://www.google.com/series/123"
+        link_fetcher = MockLinkFetcher(wrong_url)
+        with self.assertRaises(Exception) as content_manager:
+            BakaIDFinder(SERIES_TITLE, link_fetcher).get_id()
+        self.assertEqual(f"Cannot get Baka-ID from URL: {wrong_url}", str(content_manager.exception))
+
+    def test_get_id_from_url_with_no_id(self):
+        wrong_url = "https://www.mangaupdates.com/series.html"
+        link_fetcher = MockLinkFetcher(wrong_url)
+        with self.assertRaises(Exception) as content_manager:
+            BakaIDFinder(SERIES_TITLE, link_fetcher).get_id()
+        self.assertEqual(f"Cannot get Baka-ID from URL: {wrong_url}", str(content_manager.exception))
+
+class MockLinkFetcher:
+    def __init__(self, url):
+        self.url = url
+
+    def fetch(self, _series_title):
+        return self.url
